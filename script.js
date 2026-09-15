6822bcec729ba43c147cbb88e8e1135103e5ffd3
Maruyama-riki <gabrielriki1214@gmail.com>
2026-09-15 20:24:17 +0000

    2

--- script.js
+++ script.js
@@ -14,11 +14,21 @@ document.addEventListener('DOMContentLoaded', () => {
             let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeInputText}`);
             let data = await response.json();
 
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon-habitat/${pokeInputText}`);
+            let data = await response.json();
+
             let pokeImage = document.getElementById("poke-img");
             pokeImage.src = data.sprites.other["showdown"].front_default;
 
+            let pokeImage1 = document.getElementById("poke-img1");
+            pokeImage1.src = data.sprites.other["showdown"].back_default;
+
+
             let pokeImage2 = document.getElementById("poke-img2");
             pokeImage2.src = data.sprites.other["showdown"].front_shiny;
+
+            let pokeImage3 = document.getElementById("poke-img3");
+            pokeImage3.src = data.sprites.other["showdown"].back_shiny;
 
             
 
@@ -35,35 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    //API Studio Ghibli:
-    buscaButton.addEventListener("click", async () => {
-        try {
-            let filmeInputText = document.getElementById("filme").value;
-
-            if (!filmeInputText)
-                return alert("Campo de busca vazio!");
-
-            let response = await fetch("https://ghibliapi.vercel.app/films");
-            let data = await response.json();
-
-            let filmeEncontrado = data.find(filme =>
-                filme.title.toLowerCase().includes(filmeInputText.toLowerCase())
-            );
-
-            if (!filmeEncontrado) {
-                return alert("Filme não encontrado!");
-            }
-
-            document.getElementById("filme-img").src = filmeEncontrado.image;
-            document.getElementById("filme-titulo").textContent = filmeEncontrado.title;
-            document.getElementById("filme-diretor").textContent = filmeEncontrado.director;
-            document.getElementById("filme-ano").textContent = filmeEncontrado.release_date;
-            document.getElementById("filme-duracao").textContent = filmeEncontrado.running_time + " min";
-            document.getElementById("filme-nota").textContent = filmeEncontrado.rt_score;
-            document.getElementById("filme-descricao").textContent = filmeEncontrado.description;
-        } catch (error) {
-            console.log("Erro: " + error);
-        }
-    });
+    
 
 });
